6ac8c261cd344cc3c0be2b450d070eb55a1b213e
Ravi Vegad <rvegad@markettime.com>
2023-01-03 14:31:18 +0530

    slider

--- src/app/page/home/home.component.ts
+++ src/app/page/home/home.component.ts
@@ -1,25 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import Swiper from 'swiper';
 
+declare var $: any;
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
+ 
   constructor() { }
-
-  ngOnInit() {
-    var swiper = new Swiper('.swiper-container', {
-      direction: 'vertical',
-      slidesPerView: 1,
-      mousewheel: true,
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-      },
-    });
-  }
-  
+  ngOnInit()  
+  { 
+  }  
 }
